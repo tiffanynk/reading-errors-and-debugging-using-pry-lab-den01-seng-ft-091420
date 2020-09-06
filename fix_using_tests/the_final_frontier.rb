d4b68dc5988dcea673986ca831a2026aec4eb7ee
Tiffany Kanjanabout <tiffanynitk@gmail.com>
2020-09-06 18:13:26 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -4,13 +4,11 @@ def generate_star_date
   (rand(100000) + 400000) / 10.0
 end
 
-
-def state_log(star_date)
-  "Captain's Log, star date #{star_date}."
-end
-
 def engage
   puts state_log(date)
   date = generate_star_date
-  binding.pry
+end
+
+def state_log(star_date)
+  "Captain's Log, star date #{star_date}."
 end
